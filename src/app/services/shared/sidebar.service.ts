11f818df35614d9c789223d54a6bd07b1bf134ed
Alexander Andres Londoño Espejo <alexander.a.londono@accenture.com>
2020-01-21 15:26:30 -0500

    Servicio para controlar el Sidebar - Menú lateral

--- src/app/services/shared/sidebar.service.ts
+++ src/app/services/shared/sidebar.service.ts
@@ -3,6 +3,24 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class SidebarService {
 
+  menu: any = [
+    {
+      title: 'Principal',
+      icon: 'mdi mdi-gauge',
+      submenu: [
+        {
+          title: 'Dashboard', url: '/dashboard'
+        },
+        {
+          title: 'ProgressBar', url: '/progress'
+        },
+        {
+          title: 'Gráficas', url: '/graficas1'
+        },
+      ]
+    }
+  ];
+
   constructor() { }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,9 +4,15 @@ import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
 
 
 @NgModule({
+  imports: [
+    RouterModule,
+    BrowserModule
+  ],
     declarations: [
         NopagefoundComponent,
         HeaderComponent,
