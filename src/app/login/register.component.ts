793584bf28ad228ea7bd466803108ed6869e6f2e
Alexander Andres Londoño Espejo <alexander.a.londono@accenture.com>
2020-03-13 11:32:40 -0500

    Validando que las contraseñas sean iguales - ReactiveForms

--- src/app/login/register.component.ts
+++ src/app/login/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Form, FormGroup, FormControl, Validators } from '@angular/forms';
+import { group } from '@angular/core/src/animation/dsl';
 
 @Component({
   selector: 'app-register',
@@ -23,10 +24,42 @@ export class RegisterComponent implements OnInit {
       password: new FormControl(null, Validators.required),
       password2: new FormControl(null, Validators.required),
       conditions: new FormControl()
-    });
+    }, { validators: this.passwordEquals( 'password', 'password2' )} );
+
+    this.form.setValue({
+      name: 'Alexander Londoño',
+      email: 'alexlondon07@gmail.com',
+      password: 'alex',
+      password2: 'alex',
+      conditions: true
+    })
   }
 
-  registerUser(){
+  passwordEquals(field1: string, field2: string) {
+    return ( group: FormGroup ) => {
+
+      let pass1 = group.controls[field1].value;
+      let pass2 = group.controls[field2].value;
+
+      if (pass1 === pass2 ) {
+        return null;
+      }
+      return {
+        passwordEquals: true
+      };
+
+    };
+  }
+
+  registerUser() {
+    if ( this.form.invalid ) {
+      return;
+    }
+    if ( this.form.value.conditions ) {
+      console.log('Debe aceptar los términos.');
+    }
+    console.log(this.form);
+
     console.log(this.form.value);
   }
 
